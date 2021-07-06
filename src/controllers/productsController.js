3a618b469e018ada6ec01598116d72ca41799220
TheStealer <k_hintze_m@hotmail.com>
2021-07-05 19:47:07 -0500

    Formulari de creacion de productos y almacenamiento

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,55 +1,63 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
-	// Root - Show all products
-	index: (req, res) => {
-		res.render('products', {products});	
+  // Root - Show all products
+  index: (req, res) => {
+    res.render("products", { products });
+  },
 
-	},
+  // Detail - Detail from one product
+  detail: (req, res) => {
+    let productId = req.params.id;
+    console.log(productId);
+    const product = products.find((producto) => {
+      return producto.id === parseInt(productId);
+    });
+    if (product) {
+      res.render("detail", { product });
+    } else {
+      res.render("error");
+    }
+  },
 
-	// Detail - Detail from one product
-	detail: (req, res) => {
-		let productId = req.params.id;
-		console.log(productId);
-		const product = products.find((producto)=>{
-			return producto.id === parseInt(productId);
-		});
-		if(product){
-			res.render('detail',{product});
-		}else{
-			res.render('error');
-		}
-	},
+  // Create - Form to create
+  create: (req, res) => {
+    // Do the magic
+    res.render("product-create-form");
+  },
 
-	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
-	},
-	
-	// Create -  Method to store
-	store: (req, res) => {
-		// Do the magic
-	},
+  // Create -  Method to store
+  store: (req, res) => {
+    // Do the magic
+    const productInfo = req.body;
+    products.push({
+      ...productInfo,
+      id: products.length + 1,
+      image: "img-cafetera-moulinex.jpg",
+    });
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
+    res.redirect("/");
+  },
 
-	// Update - Form to edit
-	edit: (req, res) => {
-		// Do the magic
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-	},
+  // Update - Form to edit
+  edit: (req, res) => {
+    // Do the magic
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    // Do the magic
+  },
 
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
-	}
+  // Delete - Delete one product from DB
+  destroy: (req, res) => {
+    // Do the magic
+  },
 };
 
 module.exports = controller;
